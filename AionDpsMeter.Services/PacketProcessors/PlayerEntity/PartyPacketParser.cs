c1c7a83a21c7525d593243933685bbd2cbb7f55c
Kuroukihime <xddxddxdd@xdd>
2026-07-16 10:22:35 +0300

    fix: party packet parser missing u8 field resulting incorrect offset for following party members

--- AionDpsMeter.Services/PacketProcessors/PlayerEntity/PartyPacketParser.cs
+++ AionDpsMeter.Services/PacketProcessors/PlayerEntity/PartyPacketParser.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using AionDpsMeter.Services.PacketProcessors.PlayerEntity.GamePackets;
+﻿using AionDpsMeter.Services.PacketProcessors.PlayerEntity.GamePackets;
 using AionDpsMeter.Services.PacketProcessors.Shared;
 
 namespace AionDpsMeter.Services.PacketProcessors.PlayerEntity
@@ -7,17 +6,16 @@ namespace AionDpsMeter.Services.PacketProcessors.PlayerEntity
     public static class PartyPacketParser
     {
         // presence_mask bits
-        private const byte Mask_HasUnknown01 = 0x01;
-        private const byte Mask_HasGearScore = 0x02;
-        private const byte Mask_HasUnknown04 = 0x04;
-        private const byte Mask_HasUnknown08 = 0x08;
-        private const byte Mask_HasUnknown10 = 0x10;
-        private const byte Mask_HasCombatPower = 0x20;
-        private const byte Mask_HasUnknown40 = 0x40;
+        private const byte MaskHasUnknown01 = 0x01;
+        private const byte MaskHasGearScore = 0x02;
+        private const byte MaskHasUnknown04 = 0x04;
+        private const byte MaskHasUnknown08 = 0x08;
+        private const byte MaskHasUnknown10 = 0x10;
+        private const byte MaskHasCombatPower = 0x20;
+        private const byte MaskHasUnknown40 = 0x40;
 
         public static PartyPacket Parse(byte[] packet, int offset = 0)
         {
-
             var r = new PacketReader(packet);
 
             r.ReadVarInt(); //len
@@ -48,9 +46,6 @@ namespace AionDpsMeter.Services.PacketProcessors.PlayerEntity
 
         private static PartyPlayerPacket ParseMember(PacketReader r)
         {
-
-            var position = r.Position;
-
             byte mask = r.ReadU8();          // presence_mask
             byte slot = r.ReadU8();          // _number
             ulong dbid = r.ReadU64();        // _dbid
@@ -59,29 +54,30 @@ namespace AionDpsMeter.Services.PacketProcessors.PlayerEntity
             r.ReadU32();                     // unnamed, always present
             uint level = r.ReadU32();        // _level, always present
 
-            if ((mask & Mask_HasUnknown01) != 0)
-                r.ReadU32();                 // unnamed [mask&0x02]
+            if ((mask & MaskHasUnknown01) != 0)
+                r.ReadU32();                 // unnamed [mask&0x01]
 
             uint? gearScore = null;
-            if ((mask & Mask_HasGearScore) != 0)
-                gearScore = r.ReadU32();     // _equip_item_level [mask&0x04]
+            if ((mask & MaskHasGearScore) != 0)
+                gearScore = r.ReadU32();     // _equip_item_level [mask&0x02]
 
-             r.ReadBit();                      // unnamed (bit), always present
-
-            if ((mask & Mask_HasUnknown04) != 0)
+            if ((mask & MaskHasUnknown04) != 0)
                 r.ReadBit();                  // unnamed (bit) [mask&0x04]
 
+            r.ReadBit();                      // unnamed (bit), always present
 
-            if ((mask & Mask_HasUnknown08) != 0)
-                r.ReadU16();                  // unnamed (bit) [mask&0x08]
 
-            if ((mask & Mask_HasUnknown10) != 0)
+
+            if ((mask & MaskHasUnknown08) != 0)
+                r.ReadU16();                  // unnamed [mask&0x08]
+
+            if ((mask & MaskHasUnknown10) != 0)
                 r.ReadU16();                 // unnamed [mask&0x10]
 
             r.ReadU8();                      // unnamed, always present
 
             ulong? combatPower = null;
-            if ((mask & Mask_HasCombatPower) != 0)
+            if ((mask & MaskHasCombatPower) != 0)
                 combatPower = r.ReadU64();   // _combat_power [mask&0x20]
 
             var trailingArrayCount = r.ReadVarInt();
@@ -90,15 +86,17 @@ namespace AionDpsMeter.Services.PacketProcessors.PlayerEntity
             {
                 for (int j = 0; j < trailingArrayCount; j++)
                 {
+                    r.ReadU8();
                     r.ReadU32();
+                    
                 }
             }
 
-            if ((mask & Mask_HasUnknown40) != 0)
+            if ((mask & MaskHasUnknown40) != 0)
             {       
                 r.ReadU64();                 // unnamed      
             }
-            var nextMask = r.ReadU8();       // unnamed
+            r.ReadU8();       // unnamed
 
             return new PartyPlayerPacket
             {
